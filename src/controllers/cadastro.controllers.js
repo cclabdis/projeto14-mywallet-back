40f1480d0edb3a53826d7f29582181bc69cc6a01
cclabdis <cclabdis@gmail.com>
2023-07-10 17:49:31 -0300

    reenviando

--- src/controllers/cadastro.controllers.js
+++ src/controllers/cadastro.controllers.js
@@ -1,7 +1,7 @@
 import { ObjectId } from "mongodb";
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
-import dayjs from "dayjs";
+
 import { v4 as uuid } from "uuid";
 
 
@@ -9,18 +9,40 @@ export async function signUp(req, res) {
     const { name, email, password } = req.body
 
     try {
-        const user = await db.collection("users").findOne({ email })
-        if (user) return res.status(409).send("E-mail já foi usado!")
+        const userLogin = await db.collection("users").findOne({ email })
+        if (userLogin) return res.status(409).send("Unauthorized =>  E-mail já foi usado!")
 
         const hash = bcrypt.hashSync(password, 2)
 
         await db.collection("users").insertOne({ name, email, password: hash })
-        res.status(201).send("Usuário criado")
+        res.status(201).send("ok => Usuário criado")
 
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
+
+export async function login(req, res) {
+    const { email, password } = req.body
+
+    try {
+
+        //verificar se valida o email e a senha
+        const useremail = await db.collection("users").findOne({ email })
+        if (!useremail) return res.status(404).send("Unauthorized =>  E-mail não cadastrado")
+
+        const passwordCorrect = bcrypt.compareSync(password, useremail.password)
+        if (!passwordCorrect) return res.status(401).send("Unauthorized => Senha incorreta")
+
+        const token = uuid()
+        await db.collection("sessions").insertOne({ token, userId: useremail._id })
+        res.send({ token, userName: useremail.name })
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+
 
 
 //db
